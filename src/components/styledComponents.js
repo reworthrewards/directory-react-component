a7cd6182bfd399e76847291d5bd0fd8ec0d61044
Jaz Dominguez <j.dgez.p@gmail.com>
2020-12-09 11:17:16 -0600

    Add(component folder): Added folder with OfferItem and StyledComponents file

---
+++ src/components/styledComponents.js
@@ -0,0 +1,10 @@
+import { styled } from "baseui";
+
+export const Container = styled("div", {
+    padding: "1.5rem 0.7rem 0",
+});
+
+export const Flex = styled("div", {
+    display: "flex",
+    justifyContent: "space-between",
+});
